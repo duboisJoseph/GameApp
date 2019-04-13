8dd3202e41fdd1b939c5766d664043a6285fdccf
duboisJoseph <duboisj@mail.gvsu.edu>
2019-04-13 16:21:56 -0400

    AddingAsyncPeer Connection
    Base classes for allowing peers to connect and communicate Asynchronously

---
+++ GameApp/AsynchronousSocketListener.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace GameApp
+{
+  class AsynchronousSocketListener
+  {
+    public static ManualResetEvent allDone = new ManualResetEvent(false);
+    public static List<StateObject> Handlers = new List<StateObject>();
+
+    public AsynchronousSocketListener()
+    {
+    }
+
+    public static bool StartListening(IPAddress IP, int port, int handlerPool)
+    {
+      IPEndPoint localEndPoint = new IPEndPoint(IP, port);
+      Socket ListenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+      int handlerCount = 0;
+      try
+      {
+        ListenerSocket.Bind(localEndPoint);
+        ListenerSocket.Listen(100);
+
+        bool keepListening = true;
+        while (keepListening)
+        {
+          //Set event to nonsignaled state.
+          allDone.Reset();
+
+          //Start socket listening for connection
+          Console.WriteLine("Waiting for a connection...");
+          ListenerSocket.BeginAccept( new AsyncCallback(AcceptCallback), ListenerSocket);
+
+          //wait for connection to be complete
+          allDone.WaitOne();
+          handlerCount++;
+          if (handlerCount == handlerPool)
+          {
+            keepListening = false;
+          }
+        }
+      } catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+        return false;
+      }
+      ListenerSocket.Dispose();
+      return true;
+    }
+
+    private static void AcceptCallback(IAsyncResult ar)
+    {
+      // Signal the main thread to continue.  
+      allDone.Set();
+
+      // Get the socket that handles the client request.  
+      Socket listener = (Socket)ar.AsyncState;
+      Socket handler = listener.EndAccept(ar);
+
+      // Create the state object.  
+      StateObject state = new StateObject
+      {
+        workSocket = handler
+      };
+      Handlers.Add(state);
+      //listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);  
+    }
+
+    private static void EndAsyncListener(Socket handler)
+    {
+      handler.Shutdown(SocketShutdown.Both);
+      handler.Close();
+    }
+  }
+}
